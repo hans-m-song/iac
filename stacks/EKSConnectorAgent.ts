164b0516b6af5ff6e572996f730f699401274d15
Hans Song <hans.m.song@gmail.com>
2022-10-30 20:33:38 +1000

    cleanup

--- stacks/EKSConnectorAgent.ts
+++
@@ -1,7 +0,0 @@
-import * as cdk from "aws-cdk-lib";
-import { EKSConnectorAgentRole } from "@lib/iam/Role-EKS-ConnectorAgent.js";
-import { App } from "aws-cdk-lib";
-
-const app = new App();
-const stack = new cdk.Stack(app, "EKSConnectorAgent");
-new EKSConnectorAgentRole(stack);
--- stacks/HostedZoneUpdate.ts
+++ stacks/HostedZoneUpdate.ts
@@ -1,9 +1,20 @@
-import { App, Stack } from "aws-cdk-lib";
-import { HostedZoneUpdatePolicy } from "@lib/iam/Policy-Route53-HostedZoneUpdate.js";
+import { App, aws_ssm, CfnOutput, Stack, Tags } from "aws-cdk-lib";
+import { HostedZoneUpdatePolicy } from "@resources/iam/Policy-Route53-HostedZoneUpdate.js";
+import { getContext } from "@lib/aws/context.js";
 
 const app = new App();
 const stack = new Stack(app, "HostedZoneUpdate");
-new HostedZoneUpdatePolicy(stack, [
+
+const policy = new HostedZoneUpdatePolicy(stack, [
   { name: "HsongMe", id: "Z09233301OJXCBONJC133" },
   { name: "AxatolXyz", id: "Z067173715955IHMKKU3W" },
 ]);
+
+new aws_ssm.StringParameter(stack, "HostedZoneUpdatePolicyARN", {
+  parameterName: "/hosted-zone-update/policy/arn",
+  stringValue: policy.managedPolicyArn,
+  description: `ARN for managed policy ${policy.managedPolicyName}`,
+});
+
+console.log(getContext(stack));
+Tags.of(app).add("Stack", stack.stackName);
