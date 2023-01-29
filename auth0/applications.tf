ef82c354977a5791b7909a727ac336d519da92a5
Hans Song <hans.m.song@gmail.com>
2023-01-29 18:54:49 +1000

    imported resources

--- auth0/applications.tf
+++ auth0/applications.tf
@@ -1,5 +1,14 @@
-resource "auth0_client" "test_client" {
-  name        = "Test Client"
-  description = "Test Client"
-  app_type    = "non_interactive"
+resource "auth0_client" "deep-thought_minio-idp" {
+  name     = "deep-thought - Minio IDP"
+  app_type = "regular_web"
+}
+
+resource "auth0_client" "deep-thought_cluster-gateway" {
+  name     = "deep-thought cluster gateway"
+  app_type = "regular_web"
+}
+
+resource "auth0_client" "terraform_auth0-provider" {
+  name     = "Terraform Auth0 Provider"
+  app_type = "non_interactive"
 }
