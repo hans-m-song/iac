dea4df450c538db295bc463e3586195d919f33d8
Hans Song <hans.m.song@gmail.com>
2023-02-02 23:39:03 +1000

    add aws role arn var

--- github/variables.tf
+++ github/variables.tf
@@ -6,3 +6,15 @@ variable "github_token" {
 variable "github_arc_webhook_url" {
   type = string
 }
+
+variable "aws_deploy_role_arn" {
+  type = string
+}
+
+resource "github_actions_secret" "hans-m-song_iac_aws_deploy_role_arn" {
+  provider   = github.hans-m-song
+  repository = github_repository.hans-m-song_iac.name
+
+  secret_name     = "aws_deploy_role_arn"
+  plaintext_value = var.aws_deploy_role_arn
+}
