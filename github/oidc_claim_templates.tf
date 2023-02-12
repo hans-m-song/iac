f5355129b1526110ebf1af9a16fda5c4915bfe9a
Hans Song <hans.m.song@gmail.com>
2023-02-12 18:17:26 +1000

    apply oidc template to repos

--- github/oidc_claim_templates.tf
+++ github/oidc_claim_templates.tf
@@ -1,9 +1,49 @@
-resource "github_actions_repository_oidc_subject_claim_customization_template" "hans-m-song_iac" {
-  provider   = github.hans-m-song
-  repository = github_repository.hans-m-song_iac.name
+resource "github_actions_repository_oidc_subject_claim_customization_template" "axatol" {
+  for_each = toset([
+    github_repository.axatol_home-assistant-integrations.name,
+  ])
 
+  provider    = github.axatol
+  repository  = each.value
   use_default = false
+  include_claim_keys = [
+    "repo",
+    "context",
+    "workflow",
+    "job_workflow_ref",
+    "actor",
+  ]
+}
 
+resource "github_actions_repository_oidc_subject_claim_customization_template" "hans-m-song" {
+  for_each = toset([
+    github_repository.hans-m-song_iac.name,
+    github_repository.hans-m-song_kube-stack.name,
+    github_repository.hans-m-song_huisheng.name,
+  ])
+
+  provider    = github.hans-m-song
+  repository  = each.value
+  use_default = false
+  include_claim_keys = [
+    "repo",
+    "context",
+    "workflow",
+    "job_workflow_ref",
+    "actor",
+  ]
+}
+
+resource "github_actions_repository_oidc_subject_claim_customization_template" "songmatrix" {
+  for_each = toset([
+    github_repository.songmatrix_data-service.name,
+    github_repository.songmatrix_gateway.name,
+    github_repository.songmatrix_sync-service.name,
+  ])
+
+  provider    = github.songmatrix
+  repository  = each.value
+  use_default = false
   include_claim_keys = [
     "repo",
     "context",
