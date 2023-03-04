18569992527851034407acc9b73f391dbcc2878a
Hans Song <hans.m.song@gmail.com>
2023-03-04 11:39:30 +1000

    aws: add role for cloudtrail

---
+++ aws/lib/constructs/iam/CloudTrailLoggingRole.ts
@@ -0,0 +1,44 @@
+import { Stack } from "aws-cdk-lib";
+import {
+  Effect,
+  ManagedPolicy,
+  PolicyStatement,
+  Role,
+  RoleProps,
+  ServicePrincipal,
+} from "aws-cdk-lib/aws-iam";
+import { Construct } from "constructs";
+
+import { arn } from "~/lib/utils/arn";
+
+export class CloudTrailLoggingRole extends Role {
+  constructor(
+    scope: Construct,
+    id: string,
+    props?: Omit<RoleProps, "assumedBy">,
+  ) {
+    super(scope, id, {
+      ...props,
+      assumedBy: new ServicePrincipal("cloudtrail.amazonaws.com"),
+    });
+
+    const stack = Stack.of(this);
+
+    const logStreamARN = arn("*").logstream(
+      `aws-cloudtrail-logs-${stack.account}`,
+      `${stack.account}_CloudTrail_*`,
+    );
+
+    const policy = new ManagedPolicy(this, "Policy", {
+      statements: [
+        new PolicyStatement({
+          effect: Effect.ALLOW,
+          actions: ["logs:CreateLogStream", "logs:PutLogEvents"],
+          resources: [logStreamARN],
+        }),
+      ],
+    });
+
+    this.addManagedPolicy(policy);
+  }
+}
