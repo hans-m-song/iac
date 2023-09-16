ba473601caaab5b3cc3d41b4e9c8b0334647d263
Hans Song <hans.m.song@gmail.com>
2023-09-16 13:40:45 +1000

    bump terraform provider versions

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -8,17 +8,17 @@ terraform {
   required_providers {
     auth0 = {
       source  = "auth0/auth0"
-      version = "1.0.0-beta.2"
+      version = "1.0.0-beta.4"
     }
 
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.9.0"
+      version = "~> 5.17.0"
     }
 
     github = {
       source  = "integrations/github"
-      version = "~> 5.33.0"
+      version = "~> 5.37.0"
     }
 
     newrelic = {
@@ -28,7 +28,7 @@ terraform {
 
     octopusdeploy = {
       source  = "OctopusDeployLabs/octopusdeploy"
-      version = "~> 0.12.6"
+      version = "~> 0.12.7"
     }
   }
 }
