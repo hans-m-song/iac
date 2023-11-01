4d04cab815ca90523cf08c7ec260cd57c61c567f
Hans Song <hans.m.song@gmail.com>
2023-11-01 22:07:55 +1000

    update k3s startup script

--- k8s/setup-k3s.sh
+++ k8s/setup-k3s.sh
@@ -1,4 +1,9 @@
-curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=stable \
-  sh -s - \
+curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=stable sh -s - \
   --disable traefik \
-  --disable metrics-server
+  --disable metrics-server \
+  --disable local-storage \
+  --kube-apiserver-arg="oidc-issuer-url=https://axatol.au.auth0.com/" \
+  --kube-apiserver-arg="oidc-client-id=${OIDC_CLIENT_ID}" \
+  --kube-apiserver-arg="oidc-username-claim=email" \
+  --kube-apiserver-arg="oidc-username-prefix=oidc:" \
+  --kube-apiserver-arg="oidc-groups-prefix=oidc:"
