69ed29d6287703e7e54808a4b5b8c69d0216559e
Hans Song <hans.m.song+github@gmail.com>
2024-12-22 17:36:03 +1000

    update arc setup

--- k8s/setup.sh
+++ k8s/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eo pipefail
 
+brew install helm helmfile
+
 plugins=(
   https://github.com/aslafy-z/helm-git
   https://github.com/databus23/helm-diff
@@ -9,3 +11,6 @@ plugins=(
 for plugin in ${plugins[@]}; do
   helm plugin install $plugin || true
 done
+
+helmfile repos
+helmfile deps
